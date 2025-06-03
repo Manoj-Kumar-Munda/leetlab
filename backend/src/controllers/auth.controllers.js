94e1783d5ea0fe40de40a0e487c3f14bb145cf92
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2025-06-03 09:08:22 +0530

    feat: add login controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -78,10 +78,33 @@ export const login = async (req, res) => {
     if (!isValid) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
-    return res.status(200).json({ message: "Login successful" });
+    return res.status(201).json({
+      message: "User logged in successfully",
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+    });
   } catch (error) {
-    return res.status(500).json({ error: "Internal server error" });
+    return res
+      .status(500)
+      .json({ error: error.message || "Internal server error" });
   }
 };
 
